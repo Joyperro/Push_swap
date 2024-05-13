51ae2bb2b7268c846d918b85ef030e0bdec14d1e
Joyperro <86183569+Joyperro@users.noreply.github.com>
2024-05-13 18:16:05 +0200

    Add files via upload
    rotate and swap moves for the stack and the sorting of two numbers, and the file for reverse rotate moves

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,29 +6,29 @@
 /*   By: dclement <dclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 15:53:45 by dclement          #+#    #+#             */
-/*   Updated: 2024/05/07 17:53:56 by dclement         ###   ########.fr       */
+/*   Updated: 2024/05/13 17:25:11 by dclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H;
+# define PUSH_SWAP_H
 
 # define INTMAX 2147483647
 # define INTMIN -2147483648
 
 # include <stdlib.h>
 
-typedef struct s_stack_a
+typedef struct t_stack_a
 {
-    int					num;
-    struct t_stack_a	*next;
-}	t_stack_a;
+	int					num;
+	struct t_stack_a	*next;
+}						t_stack_a;
 
-typedef struct s_stack_b
+typedef struct t_stack_b
 {
-    int					num;
-    struct t_stack_b	*next;
-}	t_stack_b;
+	int					num;
+	struct t_stack_b	*next;
+}						t_stack_b;
 
 typedef struct s_stacks
 {
@@ -36,30 +36,33 @@ typedef struct s_stacks
 	struct t_stack_b	*head_b;
 	struct t_moves		*moves;
 	struct t_values		*value;
-}	t_stacks;
+}						t_stacks;
 
 typedef struct s_moves
 {
-	int	cost;
-	int	sa;
-	int	sb;
-	int	ss;
-	int	pa;
-	int	pb;
-	int	ra;
-	int	rb;
-	int	rr;
-	int	rra;
-	int	rrb;
-	int	rrr;
-}	t_moves;
+	int					cost;
+	int					sa;
+	int					sb;
+	int					ss;
+	int					pa;
+	int					pb;
+	int					ra;
+	int					rb;
+	int					rr;
+	int					rra;
+	int					rrb;
+	int					rrr;
+}						t_moves;
 
 typedef struct s_values
 {
-	int	max_a;
-	int	min_a;
-	int	max_b;
-	int	min_b;
-}	t_values;
+	int					max_a;
+	int					min_a;
+	int					max_b;
+	int					min_b;
+}						t_values;
+
+void					check_double(t_stack_a *head_a);
+void					error_check(int argc, char **argv);
 
 #endif
---
+++ push_swap/swap_nodes.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_nodes.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dclement <dclement@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 16:37:09 by dclement          #+#    #+#             */
+/*   Updated: 2024/05/13 17:28:38 by dclement         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_swap(t_stacks *stacks, char ch)
+{
+	t_stack_a	*head_a;
+	t_stack_b	*head_b;
+
+	head_a = stacks->head_a;
+	head_b = stacks->head_b;
+	if (ch == 'a')
+		ft_swap_a(head_a, 1);
+	else if (ch == 'b')
+	{
+		if (head_b == NULL || head_b->next == NULL)
+			return ;
+		ft_swap_b(head_b, 1);
+	}
+	else
+	{
+		if (head_a == NULL || head_a->next == NULL
+			|| head_b == NULL || head_b->next == NULL)
+			return ;
+		ft_swap_a(head_a, 0);
+		ft_swap_b(head_b, 0);
+		ft_printf("ss\n");
+	}
+}
+
+void	ft_swap_a(t_stack_a *head_a, int print)
+{
+	int	tmp;
+
+	tmp = head_a->num;
+	head_a->num = head_a->next->num;
+	head_a->next->num = tmp;
+	if (print != 0)
+		ft_printf("sa\n");
+}
+
+void	ft_swap_b(t_stack_b *head_b, int print)
+{
+	int	tmp;
+
+	tmp = head_b->num;
+	head_b->num = head_b->next->num;
+	head_b->next->num = tmp;
+	if (print != 0)
+		ft_printf("sb\n");
+}
